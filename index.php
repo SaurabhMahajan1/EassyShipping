23e1a44425b9c6aac56d51b058441e25ef473d52
SaurabhMahajan1 <54522792+SaurabhMahajan1@users.noreply.github.com>
2021-05-25 10:35:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="style_for_index.css">
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   
 </head>
